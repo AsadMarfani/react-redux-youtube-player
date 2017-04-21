e1aaf0b8cd27a4dc9af61578cb6520c858b61508
Asad Marfani <Asad Marfani>
2017-04-22 02:02:03 +0500

    Initial Commit

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import logo from '../images/logo.svg';
+import '../styles/App.css';
+
+
+class App extends Component {
+  constructor() {
+    super();
+  }
+  render() {
+    return (
+      <div className = "container">
+        <h1>React-Redux Starter Kit</h1>
+      </div>
+    );
+  } 
+}
+export default App;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+
+import App from './components/App';
+import './styles/index.css';
+
+ReactDOM.render(
+    <App/>,
+  document.getElementById('root')
+);
